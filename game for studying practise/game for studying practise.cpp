132224041919911822c1d1c9c6ce87924eaef2c2
Yurii Shymkiv <dohewghs@gmail.com>
2026-03-17 12:46:38 +0200

    Add project files.

---
+++ game for studying practise/game for studying practise.cpp
@@ -0,0 +1,12 @@
+﻿// game for studying practise.cpp : Defines the entry point for the application.
+//
+
+#include "game for studying practise.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ game for studying practise/game for studying practise.h
@@ -0,0 +1,8 @@
+﻿// game for studying practise.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
