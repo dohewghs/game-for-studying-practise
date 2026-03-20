3db983cecc1ac6141828a05f3adc413d0bf38147
dohewghs <dohewghs@gmail.com>
2026-03-20 11:18:29 +0200

    add Vector2

---
+++ StudyGame/Vector2.h
@@ -0,0 +1,18 @@
+class Vector2
+{
+public:
+	double x;
+	double y;
+
+	Vector2(double _x = 0, double _y = 0) :
+		x(_x),
+		y(_y)
+	{
+	};
+
+	Vector2(const Vector2& other) :
+		x(other.x),
+		y(other.y)
+	{
+	};
+};
