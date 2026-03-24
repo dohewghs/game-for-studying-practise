f9a02db3783e8883d19db7771b9c64e9e39cb5e9
Yurii Shymkiv <dohewghs@gmail.com>
2026-03-24 14:06:29 +0200

    delete gravitationScale and friction in RigidBody

--- StudyGame/RigidBody.h
+++ StudyGame/RigidBody.h
@@ -10,19 +10,15 @@ class RigidBody
 {
 private:
 	Rect* hitBox;
-	double gravityScale;
 
 	Vector2 velocity;
 	Vector2 acceleration;
-	double friction;
 
 public:
 	RigidBody(Rect* _hitbox = nullptr) :
 		hitBox(_hitbox),
-		gravityScale(1),
 		velocity(0,0),
-		acceleration(0,0),
-		friction(0.1)
+		acceleration(0,0)
 	{
 	}
 
@@ -35,14 +31,10 @@ public:
 	void update(float deltaTime)
 	{
 		velocity += acceleration * deltaTime;
-		velocity *= (1 - this->friction);
-
 
 		hitBox->x += velocity.x * deltaTime;
 		hitBox->y += velocity.y * deltaTime;
 
 		acceleration = Vector2(0, 0);
 	}
-
-
 };
