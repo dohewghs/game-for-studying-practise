1d63c63f3eabcdf8dbf04d8050638431b297e2b5
Yurii Shymkiv <dohewghs@gmail.com>
2026-04-12 11:00:09 +0300

    make weapon with present

--- StudyGame/Weapon.cpp
+++ StudyGame/Weapon.cpp
@@ -1,6 +1,38 @@
 #include "Weapon.h"
 #include "Entity.h"
 
-void Weapon::use(Entity entity)
+void Weapon::update(double deltaTime)
 {
+	if (this->rotateWeapon)
+	{
+		this->angle += 5 * deltaTime;
+	}
+
+	if (angle > 110)
+	{
+		angle = 0;
+		this->rotateWeapon = false;
+	}
+}
+
+void Weapon::setCoords(Vector2 coords)
+{
+	this->hitBox.x = coords.x;
+	this->hitBox.y = coords.y;
+}
+
+void Weapon::present(SDL_Renderer* renderer)
+{
+	if (!this->presenter)
+		return;
+
+	this->presenter->present(renderer, this);
+}
+
+void Weapon::rotate()
+{
+	if (this->rotateWeapon)
+		return;
+
+	this->rotateWeapon = true;
 }
--- StudyGame/Weapon.h
+++ StudyGame/Weapon.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "BaseWeaponPresenter.h"
+#include "Vector2.h"
 
 class Entity;
 
@@ -6,11 +8,34 @@ class Weapon
 {
 private:
 	double damage;
+
+	Rect hitBox;
+	double angle;
+	bool rotateWeapon;
+
+	IWeaponPresenter* presenter;
+
 public:
 	Weapon() :
-		damage(10)
-	{ };
-	~Weapon() = default;
+		damage(10),
+		hitBox({0,0,50,7}),
+		angle(10),
+		rotateWeapon(false),
+		presenter(new BaseWeaponPresenter(&this->hitBox, &this->angle))
+	{
+	};
+
+	~Weapon()
+	{
+		if (this->presenter)
+			delete this->presenter;
+	}
 	
-	void use(Entity entity);
+	void update(double deltaTime);
+
+	void setCoords(Vector2 coords);
+
+	void present(SDL_Renderer* renderer);
+
+	void rotate();
 };
